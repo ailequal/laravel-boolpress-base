3d39d1ef328fcaef0e35700f5838f74368645562
ailequal <37016865+ailequal@users.noreply.github.com>
2020-03-25 21:27:59 +0100

    index() is working

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -13,7 +14,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view('posts.index');
+        // requesting all the posts from the db
+        $posts = Post::all();
+        return view('posts.index', ["posts"=>$posts]);
     }
 
     /**
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,30 +4,30 @@
 <div class="posts container">
 	<h2>Posts</h2>
 	<div class="wrapper">
+		@foreach ($posts as $post)
 		<div class="post">
-			<h2>title</h2>
-			<h3>subtitle</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum provident quam itaque corporis quisquam
-				adipisci accusamus explicabo, asperiores voluptatibus, sequi inventore nobis esse ipsa nostrum sit odit,
-				molestiae a sapiente!</p>
+			<h2>{{$post->title}}</h2>
+			<h3>{{$post->subtitle}}</h3>
+			<p>{{$post->text}}</p>
 			<div class="box">
 				<div class="authors">
-					<span class="author">author</span>
-					<span class="collaborator">collaborator</span>
+					<span class="author">{{$post->author}}</span>
+					<span class="collaborator">{{$post->collaborator}}</span>
 				</div>
 				<div class="info">
-					<span class="language">language</span>
-					<span class="topic">topic</span>
+					<span class="language">{{$post->language}}</span>
+					<span class="topic">{{$post->topic}}</span>
 				</div>
 				<div class="link">
-					<a class="external_link" href="#">external_link</a>
+					<a class="external_link" href="#">{{$post->external_link}}</a>
 				</div>
 				<div class="date">
-					<span class="created_at">created_at</span>
-					<span class="updated_at">updated_at</span>
+					<span class="created_at">{{$post->created_at}}</span>
+					<span class="updated_at">{{$post->updated_at}}</span>
 				</div>
 			</div>
 		</div>
+		@endforeach
 	</div>
 </div>
 @endsection
